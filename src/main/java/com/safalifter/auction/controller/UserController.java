486f48c80270d7d4fb66d2d35320ac8b89dc6dfe
safalifter <70344201+safalifter@users.noreply.github.com>
2023-03-17 18:36:07 +0300

    modelmapper added

--- src/main/java/com/safalifter/auction/controller/UserController.java
+++ src/main/java/com/safalifter/auction/controller/UserController.java
@@ -1,28 +1,33 @@
 package com.safalifter.auction.controller;
 
-import com.safalifter.auction.model.User;
+import com.safalifter.auction.dto.UserDto;
 import com.safalifter.auction.request.UserCreateRequest;
 import com.safalifter.auction.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final ModelMapper modelMapper;
 
     @PostMapping
-    ResponseEntity<User> create(@RequestBody UserCreateRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(request));
+    ResponseEntity<UserDto> create(@RequestBody UserCreateRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(modelMapper.map(userService.create(request), UserDto.class));
     }
 
     @GetMapping
-    ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.ok(userService.getUsers());
+    ResponseEntity<List<UserDto>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers().stream()
+                .map(x -> modelMapper.map(x, UserDto.class)).collect(Collectors.toList()));
     }
 }
---
+++ src/main/java/com/safalifter/auction/dto/UserDto.java
@@ -0,0 +1,10 @@
+package com.safalifter.auction.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String username;
+    private String profileImage;
+}
