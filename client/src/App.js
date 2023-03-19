fded1596f5d220535223ff7d8f94860ba25f474d
safalifter <70344201+safalifter@users.noreply.github.com>
2023-03-19 11:54:16 +0300

    redux added

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,23 @@ import AuthPage from "./pages/AuthPage";
 import {HelmetProvider} from "react-helmet-async";
 import {BrowserRouter} from "react-router-dom";
 import ThemeProvider from './theme';
+import NavbarLayout from "./layouts/navbar";
+import HomePage from "./pages/HomePage";
+import {useSelector} from "react-redux";
 
 function App() {
+    const currentUser = useSelector(state => state.reduxSlice.currentUser)
     return (
         <HelmetProvider>
             <BrowserRouter>
                 <ThemeProvider>
-                    <AuthPage/>
+                    {!currentUser ?
+                        <AuthPage/> :
+                        <>
+                            <NavbarLayout/>
+                            <HomePage/>
+                        </>
+                    }
                 </ThemeProvider>
             </BrowserRouter>
         </HelmetProvider>
---
+++ client/src/redux/reduxSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+export const reduxSlice = createSlice({
+    name: 'redux',
+    initialState: {
+        currentUser: localStorage.getItem('currentUser'),
+        accessToken: localStorage.getItem('accessToken'),
+    },
+    reducers: {
+        login: (state, action) => {
+            localStorage.setItem('currentUser', action.payload.username)
+            localStorage.setItem('accessToken', action.payload.accessToken)
+            state.currentUser = action.payload.username
+            state.accessToken = action.payload.accessToken
+        },
+        logout: state => {
+            localStorage.removeItem('currentUser')
+            localStorage.removeItem('accessToken')
+            state.currentUser = undefined
+            state.accessToken = undefined
+        },
+    }
+})
+export const {login, logout} = reduxSlice.actions
+export default reduxSlice.reducer
