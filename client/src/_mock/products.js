3958b532e1ff2e305aaab70ba5df1a4877a9f162
safalifter <70344201+safalifter@users.noreply.github.com>
2023-03-27 01:16:36 +0300

    mock updated

--- client/src/_mock/products.js
+++ client/src/_mock/products.js
@@ -1,39 +1,41 @@
-import {faker} from '@faker-js/faker';
-
 // ----------------------------------------------------------------------
 
 const PRODUCTS = [
     {
+        id: 1,
         name: 'Gibson Les Paul',
         price: 4999,
         color: '#090A0A',
-        link: 'https://www.gibson.com/en-US'
+        link: 'https://www.gibson.com/en-US',
+        cover: `/assets/images/products/product_1.jpg`
     },
     {
+        id: 2,
         name: 'Fender Stratocaster',
         price: 2849,
         color: '#EFF5F5',
-        link: 'https://www.fender.com/en-US'
+        link: 'https://www.fender.com/en-US',
+        cover: `/assets/images/products/product_2.jpg`
     },
     {
+        id: 3,
         name: 'Jackson Premium',
         price: 1467,
         color: '#4D5050',
-        link: 'https://www.jacksonguitars.com/en'
+        link: 'https://www.jacksonguitars.com/en',
+        cover: `/assets/images/products/product_3.jpg`
     },
 ];
 // ----------------------------------------------------------------------
 
 const products = [...Array(3)].map((_, index) => {
-    const setIndex = index + 1;
-
     return {
-        id: faker.datatype.uuid(),
-        cover: `/assets/images/products/product_${setIndex}.jpg`,
+        id: PRODUCTS[index].id,
         name: PRODUCTS[index].name,
         price: PRODUCTS[index].price,
         color: PRODUCTS[index].color,
-        link: PRODUCTS[index].link
+        link: PRODUCTS[index].link,
+        cover: PRODUCTS[index].cover,
     };
 });
 
