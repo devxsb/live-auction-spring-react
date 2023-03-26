ab889d16b88b97151ab1769eaf05c8a5e41aba2a
safalifter <70344201+safalifter@users.noreply.github.com>
2023-03-26 14:59:44 +0300

    modelmapper config updated

--- src/main/java/com/safalifter/auction/config/BeanConfig.java
+++ src/main/java/com/safalifter/auction/config/BeanConfig.java
@@ -1,7 +1,10 @@
 package com.safalifter.auction.config;
 
+import org.hibernate.collection.spi.PersistentCollection;
+import org.modelmapper.Condition;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,12 +15,18 @@ public class BeanConfig {
     @Bean
     public ModelMapper getModelMapper() {
         ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+        modelMapper.getConfiguration().setPropertyCondition(new Condition<Object, Object>() {
+            public boolean applies(MappingContext<Object, Object> context) {
+                return !(context.getSource() instanceof PersistentCollection);
+            }
+        }); // will be solved
         return modelMapper;
     }
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+        @Bean
+        public BCryptPasswordEncoder bCryptPasswordEncoder () {
+            return new BCryptPasswordEncoder();
+        }
     }
-}
