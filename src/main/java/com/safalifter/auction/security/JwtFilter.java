2e5b33f9c7fc8e9c37815cef7514bc5ae93591df
safalifter <70344201+safalifter@users.noreply.github.com>
2023-03-29 01:03:08 +0300

    jwt improved

--- src/main/java/com/safalifter/auction/security/JwtFilter.java
+++ src/main/java/com/safalifter/auction/security/JwtFilter.java
@@ -1,11 +1,8 @@
 package com.safalifter.auction.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.safalifter.auction.service.TokenService;
 import com.safalifter.auction.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,33 +22,33 @@ import java.util.Map;
 @Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
-    private final TokenService tokenService;
+    private final JwtUtil jwtUtil;
     private final UserDetailsServiceImpl userDetailsService;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
-
-        String header = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (header == null || !header.startsWith("Bearer ")) {
-            filterChain.doFilter(request, response);
-            return;
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        String jwt = null;
+        String username = null;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            try {
+                jwt = authHeader.substring(7);
+                username = jwtUtil.extractUsername(jwt);
+            } catch (Exception e) {
+                sendError(response, e);
+                return;
+            }
         }
-        String token = header.substring(7);
-        String username;
-        try {
-            username = tokenService.verifyJWT(token).getSubject();
-        } catch (Exception e) {
-            sendError(response, e);
-            return;
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (jwtUtil.verifyJWT(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
         }
-        if (username == null) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         filterChain.doFilter(request, response);
     }
 
