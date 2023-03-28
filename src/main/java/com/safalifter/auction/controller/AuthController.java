93b64b8f6ea67bda5a8648b69d8615b7669a7692
safalifter <70344201+safalifter@users.noreply.github.com>
2023-03-29 01:02:26 +0300

    logout service created

--- src/main/java/com/safalifter/auction/controller/AuthController.java
+++ src/main/java/com/safalifter/auction/controller/AuthController.java
@@ -1,16 +1,14 @@
 package com.safalifter.auction.controller;
 
-import com.safalifter.auction.dto.TokenDTO;
 import com.safalifter.auction.dto.UserDto;
+import com.safalifter.auction.model.Token;
 import com.safalifter.auction.request.LoginRequest;
 import com.safalifter.auction.request.RegisterRequest;
 import com.safalifter.auction.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -19,14 +17,21 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final ModelMapper modelMapper;
 
     @PostMapping("/login")
-    ResponseEntity<TokenDTO> handleLogin(@RequestBody LoginRequest request) {
+    ResponseEntity<Token> handleLogin(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
     @PostMapping("/signup")
     ResponseEntity<UserDto> handleSignUp(@Valid @RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authService.signup(request));
+        return ResponseEntity.ok(modelMapper.map(authService.signup(request), UserDto.class));
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader(name = "Authorization") String auth) {
+        authService.logout(auth);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/safalifter/auction/service/AuthService.java
+++ src/main/java/com/safalifter/auction/service/AuthService.java
@@ -1,12 +1,12 @@
 package com.safalifter.auction.service;
 
-import com.safalifter.auction.dto.TokenDTO;
-import com.safalifter.auction.dto.UserDto;
 import com.safalifter.auction.exc.WrongCredentialsException;
+import com.safalifter.auction.model.Token;
+import com.safalifter.auction.model.User;
 import com.safalifter.auction.request.LoginRequest;
 import com.safalifter.auction.request.RegisterRequest;
+import com.safalifter.auction.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,26 +16,33 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager;
     private final UserService userService;
     private final TokenService tokenService;
-    private final ModelMapper modelMapper;
 
-    public TokenDTO login(LoginRequest request) {
+    public Token login(LoginRequest request) {
         try {
             Authentication auth = authenticationManager
                     .authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-            return TokenDTO
-                    .builder()
-                    .accessToken(tokenService.generateToken(auth))
-                    .username(request.getUsername())
-                    .build();
+            final String jwt = jwtUtil.generateToken(auth);
+            return tokenService.saveToken(
+                    Token.builder()
+                            .jwt(jwt)
+                            .username(request.getUsername()).build(),
+                    jwtUtil.tokenExpiredHours(jwt));
         } catch (final BadCredentialsException badCredentialsException) {
             throw new WrongCredentialsException("Invalid Username or Password");
         }
     }
 
-    public UserDto signup(RegisterRequest request) {
-        return modelMapper.map(userService.create(request), UserDto.class);
+    public User signup(RegisterRequest request) {
+        return userService.create(request);
+    }
+
+    public void logout(String auth) {
+        String jwt = auth.substring(6);
+        String username = jwtUtil.extractUsername(jwt);
+        tokenService.delete(username);
     }
 }
